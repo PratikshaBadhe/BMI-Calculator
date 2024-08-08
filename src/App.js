11dfbe7d6647ec43467bdbacaa45353eca130937
badhe pratiksha <badhepratiksha798@gmial.com>
2024-08-08 15:29:39 +0530

    bmi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
+// App.js
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+  const [bmi, setBmi] = useState('');
+  const [message, setMessage] = useState('');
+
+  const calcBmi = (event) => {
+    event.preventDefault();
+    if (weight === '' || height === '') {
+      alert('Please enter a valid weight and height');
+    } else {
+      const bmi = (weight /(height)).toFixed(1);
+      setBmi(bmi);
+      setMessage(bmi < 25 ? 'You are underweight' : (bmi < 30 ? 'You are a healthy weight' : 'You are overweight'));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h2 className="center">BMI Calculator</h2>
+        <form onSubmit={calcBmi}>
+          <div>
+            <label>Weight (kg)</label>
+            <input value={weight} onChange={(e) => setWeight(e.target.value)} type="number" />
+          </div>
+          <div>
+            <label>Height (cm)</label>
+            <input value={height} onChange={(e) => setHeight(e.target.value)} type="number" />
+          </div>
+          <div>
+            <button className="btn" type="submit">Submit</button>
+            <button className="btn btn-outline" type="button" onClick={() => window.location.reload()}>Reload</button>
+          </div>
+        </form>
+        <div className="center">
+          <h3>Your BMI is: {bmi}</h3>
+          <p>{message}</p>
+        </div>
+      </div>
     </div>
   );
 }
